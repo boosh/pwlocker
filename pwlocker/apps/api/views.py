4df68583c41edbf355086744169b562b5f2e521b
Boosh <al@albates.co.uk>
2012-05-27 12:35:21 +0100

    Restrict users to their own data in a more elegant way

--- pwlocker/apps/api/views.py
+++ pwlocker/apps/api/views.py
@@ -1,34 +1,39 @@
+from djangorestframework.mixins import ModelMixin
 from djangorestframework.permissions import IsAuthenticated
 from djangorestframework.views import ListOrCreateModelView, InstanceModelView
 
 from apps.passwords.resources import PasswordResource
 
-class PasswordListView(ListOrCreateModelView):
+class RestrictToUserMixin(ModelMixin):
     """
-    List view for Password objects.
+    Mixin that restricts users to working with their own data
     """
-    resource = PasswordResource
-    permissions = (IsAuthenticated, )
-
     def get_queryset(self):
         """
         Only return objects created by the currently authenticated user.
         """
         return self.resource.model.objects.filter(created_by=self.user)
 
-    def post(self, request, *args, **kwargs):
+    def get_instance_data(self, model, content, **kwargs):
         """
-        self.user is a property created by the AuthMixin. So we can simply
-        update the submitted content dict to set the created_by field to the
-        currently authenticated user.
+        Set the created_by field to the currently authenticated user.
         """
-        self.CONTENT['created_by'] = self.user
-        return super(PasswordListView, self).post(request, *args, **kwargs)
+        content['created_by'] = self.user
+        return super(RestrictToUserMixin, self).get_instance_data(model, content, **kwargs)
 
 
-class PasswordInstanceView(InstanceModelView):
+class PasswordListView(RestrictToUserMixin, ListOrCreateModelView):
+    """
+    List view for Password objects.
+    """
+    resource = PasswordResource
+    permissions = (IsAuthenticated, )
+
+
+class PasswordInstanceView(RestrictToUserMixin, InstanceModelView):
     """
     View for individual Password instances
     """
     resource = PasswordResource
     permissions = (IsAuthenticated, )
+
